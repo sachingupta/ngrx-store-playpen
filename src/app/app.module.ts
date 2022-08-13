d9f354eb35c2aec44f7d2aa9922d801cc7df140a
sachingupta <hapysachin91@gmail.com>
2022-08-12 18:29:41 -0700

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 
 import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  providers: [],
+  declarations: [AppComponent, MyCounterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
 
 /*
 Use of this source code is governed by an MIT-style license that
